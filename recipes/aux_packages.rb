63fcbd000abcb15211e746ce26b8600a501e0079
Jason Blalock <jasonblalock@users.noreply.github.com>
2016-03-05 01:54:15 -0500

    Add aux_packages recipe that installs vim and add tests for default.rb

---
+++ recipes/aux_packages.rb
@@ -0,0 +1,9 @@
+#
+# Cookbook Name:: .
+# Recipe:: aux_packages
+#
+# Copyright (c) 2016 Jason Blalock, All Rights Reserved.
+
+include_recipe 'apt'
+
+package 'vim'
--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -6,4 +6,33 @@ describe 'dev_box::default' do
   it 'does something' do
     skip 'Replace this with meaningful tests'
   end
+
+  describe 'dev_box::aux_packages' do
+    describe package('vim') do
+      it 'is installed' do
+        expect(subject).to be_installed
+      end
+    end
+  end
+
+  describe 'dev_box::main_user' do
+    describe user('jason') do
+      it 'exists' do
+        expect(subject).to exist
+      end
+
+      it 'belongs to the correct groups' do
+        expect(subject).to belong_to_group('jason')
+        expect(subject).to belong_to_group('sudo')
+      end
+
+      it 'has correct home' do
+        expect(subject).to have_home_directory('/home/jason')
+      end
+
+      it 'has correct shell' do
+        expect(subject).to have_login_shell('/bin/bash')
+      end
+    end
+  end
 end
