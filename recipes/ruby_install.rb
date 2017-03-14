a11fe6fe2d941d0dafb68ab52f746484e201e7ea
Jason Blalock <jasonblalock@users.noreply.github.com>
2017-03-14 12:21:48 -0400

    Update ruby_install to install version 2.4

--- recipes/ruby_install.rb
+++ recipes/ruby_install.rb
@@ -2,8 +2,8 @@ dev_box_user = node['dev_box']['user']
 
 include_recipe 'ruby_install::default'
 
-ruby_install_ruby 'ruby 2.3' do
-  ruby 'ruby 2.3'
+ruby_install_ruby 'ruby 2.4' do
+  ruby 'ruby 2.4'
   user dev_box_user
   group dev_box_user
   environment ({ 'HOME' => "/home/#{dev_box_user}", 'USER' => dev_box_user })
